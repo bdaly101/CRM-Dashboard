356a39b3afd1c2304ac281efcdeb59e0e917ad98
Brendan <daly2b@gmail.com>
2024-01-15 19:20:03 -0500

    Renamed usernameRoutes to homeRoutes and fixed homeRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const usernameRoutes = require('./usernameRoutes');
+const homeRoutes = require('./homeRoutes');
 
-router.use('/', usernameRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
