8f388b2575416952199843a2bd104127954bb552
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-01-17 13:14:07 -0500

    made delete contact btn appear and disappear given selection

--- public/js/checkboxes.js
+++ public/js/checkboxes.js
@@ -6,12 +6,33 @@ const checkFcn = async (event) => {
   console.log(topCheck)
   console.log(topCheck.checked)
   const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
-  console.log(checkBoxes);
+  console.log(checkBoxes.length);
   for (let i=1; i<checkBoxes.length; i++) {
     checkBoxes[i].checked = topCheck.checked;
+  }
+  if(topCheck.checked) {
+    deleteBtn.classList.remove("invisible")
+  }
+  else {
+    deleteBtn.classList.add("invisible")
   }
 }
 
 document
   .querySelector('#checkboxall')
   .addEventListener('click', checkFcn)
+
+const deleteBtn = document.querySelector('#delete-contact-btn')
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+for (let i =1; i< checkboxes.length; i++) {
+  checkboxes[i].addEventListener('click', () => {
+    const checkedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked')
+    console.log(checkedCheckboxes.length)
+    if (checkedCheckboxes.length > 0) {
+      deleteBtn.classList.remove("invisible")
+    }
+    else {
+      deleteBtn.classList.add("invisible")
+    }
+  })
+}
